89e65946ad2993b814ec3a8c68624ae845f5d659
Kevin Kim <kevink@squareup.com>
2022-01-26 22:47:32 -0800

    Add cli command for eclipse artifact generation

--- cmd/bea/main.go
+++ cmd/bea/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-	fmt.Println("Hello")
+	app := &cli.App{
+		Name:    "bea",
+		Version: "0.1",
+		Usage:   "Bazel Eclipse Adapter",
+		Commands: []*cli.Command{
+			{
+				Name:    "generate",
+				Aliases: []string{"g"},
+				Usage: `Generates Eclipse artifacts (i.e .project / .classpath) for a
+                Bazel workspace to work with Eclipse IDEs (including the language server).`,
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
